d59c7633fe8e0b474949c714bbea06953797da81
Rikhil Amonkar <rikhilma@gmail.com>
2025-04-13 19:39:35 -0400

    All main frontend for risk page, form page, and about page has been made and the UI is fully developed. The routes are all completed and some JS has been implemented to connect buttons to pages.

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,6 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("get-started-button");
+
+    if (button) {
+        button.addEventListener("click", function () {
+            window.location.href = "/risk";
+        });
+    }
+});
+
 document.getElementById("drug-response-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
     document.getElementById("display-percentage-container").style.display = "block";
+
+});
+
+document.getElementById("cancer-risk-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    document.getElementById("empty-rec-text").style.display = "none";
 
 });
